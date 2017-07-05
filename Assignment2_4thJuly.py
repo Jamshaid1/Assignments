1f9a81465e4402908b3f5837b6c4ac14756893b5
Jamshaid <Jamshaid1>
2017-07-05 04:12:27 -0400

    first commit

--- Assignment2_4thJuly.py
+++ Assignment2_4thJuly.py
@@ -6,4 +6,15 @@
 # input number. The order in which the equations are printed matters.
 
 def print_multiplication_table( n ):
+   num=1
+   for i in range(1,n+1):
+    print num,"*",i,"=",num*i
+   for i in range(1,n+1):
+    print (num+1),"*",i,"=",(num+1)*i
 
+   return
+
+print_multiplication_table(2)
+print 
+
+print_multiplication_table(3)
